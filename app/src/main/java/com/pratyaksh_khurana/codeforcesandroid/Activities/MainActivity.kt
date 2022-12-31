da50fccbe56580e44adf73dfc6c267dbbd01661e
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-12-31 22:49:05 +0530

    Fix include
    - Add Splash screen

--- app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/MainActivity.kt
+++ app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.pratyaksh_khurana.codeforcesandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
     }
 }
