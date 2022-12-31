4eca8fd733c7520f3a7d52231d1918af84eab069
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2023-01-01 00:14:50 +0530

    Fix include
    - Add icons
    - Add fonts

--- app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/MainActivity.kt
+++ app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/MainActivity.kt
@@ -2,11 +2,16 @@ package com.pratyaksh_khurana.codeforcesandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pratyaksh_khurana.codeforcesandroid.Fragments.ContestsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ContestsFragment())
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onBackPressed() {
