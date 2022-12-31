510998fc0653bbcdb8380410c83526c111624bfc
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-12-31 20:16:05 +0530

    initial setup

---
+++ app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pratyaksh_khurana.codeforcesandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
