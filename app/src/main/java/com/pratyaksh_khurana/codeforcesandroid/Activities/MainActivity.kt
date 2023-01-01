a7e0c35ebe48139920f9dfef1a7832a7d824f2f9
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2023-01-01 23:30:01 +0530

    Fix include
    - Add fragments - Contests, Users, News, Problems
    - Add more icons of fragments

--- app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/MainActivity.kt
+++ app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/MainActivity.kt
@@ -3,17 +3,47 @@ package com.pratyaksh_khurana.codeforcesandroid
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pratyaksh_khurana.codeforcesandroid.Fragments.ContestsFragment
+import com.pratyaksh_khurana.codeforcesandroid.Fragments.NewsFragment
+import com.pratyaksh_khurana.codeforcesandroid.Fragments.ProblemsFragment
+import com.pratyaksh_khurana.codeforcesandroid.Fragments.UsersFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Default navigation
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, ContestsFragment())
-            .addToBackStack(null)
+            .addToBackStack("fragment_contests")
             .commit()
-    }
 
-    override fun onBackPressed() {
+        contests_bottom_bar.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ContestsFragment())
+                .addToBackStack("fragment_contests")
+                .commit()
+        }
+
+        problems_bottom_bar.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ProblemsFragment())
+                .addToBackStack("fragment_problems")
+                .commit()
+        }
+
+        users_bottom_bar.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, UsersFragment())
+                .addToBackStack("fragment_users")
+                .commit()
+        }
+        news_bottom_bar.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, NewsFragment())
+                .addToBackStack("fragment_news")
+                .commit()
+        }
     }
 }
--- app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/SplashScreenActivity.kt
+++ app/src/main/java/com/pratyaksh_khurana/codeforcesandroid/Activities/SplashScreenActivity.kt
@@ -10,6 +10,7 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
         }, 1000)
